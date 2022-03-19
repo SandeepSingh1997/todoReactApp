5207548bb2b7d9f1f5413e34bb14db19c365acf2
SandeepSingh1997 <sandeepsingh10101997@gmail.com>
2022-03-19 13:48:41 +0530

    added focus feature

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 export default function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
 
   const editFieldRef = useRef(null);
   const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
 
   function toggleIsEditing() {
     setEditing(true);
@@ -98,12 +108,12 @@ export default function Todo(props) {
     </div>
   );
   useEffect(() => {
-    if (isEditing) {
+    if (!wasEditing && isEditing) {
       editFieldRef.current.focus();
-    } else {
+    }
+    if (wasEditing && !isEditing) {
       editButtonRef.current.focus();
     }
-  }, [isEditing]);
-  console.log("main render");
+  }, [wasEditing, isEditing]);
   return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
